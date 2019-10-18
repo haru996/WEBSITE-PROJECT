765270f4d222c86cabb42b50c3f6770c82b9b97b
haru996 <49772785+haru996@users.noreply.github.com>
2019-10-18 11:54:50 +0800

    Initial commit

---
+++ OVERVIEW.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#contact").click(function(){
+        $("#us").slideToggle("slow");
+    });
+});
+$(document).ready(function(){
+    $("#open").click(function(){
+        $("#day").slideToggle("slow");
+    });
+});
